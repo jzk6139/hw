b77fdcc5a0f637b11f67fd28aa2641cd7ddfe072
jzk6139 <jzk6139@psu.edu>
2020-08-31 05:24:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+temperature = input("Enter temperature in Celsius: ")
+Celsius = temperature 
+Celsius = int(temperature)
+Celsius = (Celsius * 9//5)
+Celsius = (Celsius + 32)
+Celsius = str(Celsius)
+print (Celsius) 
